fe7ceff6142919e18f757607398a25d7c24a9f68
Guilherme Lisboa <guibrbr161@gmail.com>
2023-09-04 00:19:40 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/Lisbooa16/goapi/router"
+import (
+	"github.com/Lisbooa16/goapi/config"
+	"github.com/Lisbooa16/goapi/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	// Initializer configs
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initializer error: %v", err)
+		return
+	}
 
 	// Initializer router
 	router.Initializer()
